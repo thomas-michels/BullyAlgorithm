fd7e10d67ed25386f8764894a0d8bfcc95ec5cc1
Thomas Michels Rodrigues <thomas.rodrigues@ambevtech.com.br>
2022-03-18 14:18:50 -0300

    Process class

---
+++ app/process/__init__.py
@@ -0,0 +1,5 @@
+"""
+Process module
+"""
+
+from app.process.process import Process
---
+++ app/process/process.py
@@ -0,0 +1,42 @@
+"""
+Process module
+"""
+
+from typing import NoReturn
+from app.utils import GenerateID
+
+
+class Process:
+
+    __id: int
+    __is_active: bool = False
+
+    def __init__(self) -> None:
+        self.__id = GenerateID().get_id()
+
+    def get_id(self) -> int:
+        """
+        Method to get id of Process
+
+        :return:
+            int
+        """
+        return self.__id
+
+    def change_status(self) -> NoReturn:
+        """
+        Method to change status of process
+
+        :return:
+            NoReturn
+        """
+        self.__is_active = not self.__is_active
+
+    def is_active(self) -> bool:
+        """
+        Method to check if process is active
+
+        :return:
+            Bool
+        """
+        return self.__is_active
